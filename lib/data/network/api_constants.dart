92bc8679d3c101ae68003247ca5d7504e7799996
manipoint <manipoint@gmail.com>
2023-08-26 03:39:46 +0500

    change

--- lib/data/network/api_constants.dart
+++ lib/data/network/api_constants.dart
@@ -1,7 +1,7 @@
 import 'package:movies_planet/resources/mp_season_detail_params.dart';
 
 class MpApi {
-  static const String apiKey = 'd77f8746e68b96e9934dde598cd87cb3';
+  static const String apiKey = '';
   static const String baseUrl = 'https://api.themoviedb.org/3';
 
   static const String baseBackdropUrl = 'https://image.tmdb.org/t/p/w1280';
