edbed2f640f653d8ed55038047e238ca068e4777
manipoint <manipoint@gmail.com>
2023-08-26 03:37:13 +0500

    first

---
+++ lib/presentation/views/movies/top_rated_movies_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TopRatedMoviesView extends StatelessWidget {
+  const TopRatedMoviesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Top rated movies View"),);
+  }
+}
---
+++ lib/presentation/views/tv/top_rated_show_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TopRatedShowsView extends StatelessWidget {
+  const TopRatedShowsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Top rated Shows View"),);
+  }
+}
